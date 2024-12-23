5abe1d9e2a01a263f45756508431012b1acb3f1a
Ray <raypeaslee@gmail.com>
2024-12-23 18:04:58 -0500

    add a bootstrap theme
    https://bootswatch.com/united/

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -1,10 +1,11 @@
 import { type Hole, html } from '../lib/view'
+//import "bootswatch/dist/united/bootstrap.min.css"
 
 export function shell({ title, content }: { title: string; content: Hole }) {
   return html`<html>
     <head>
       <title>${title}</title>
-      <link rel="stylesheet" href="/public/styles.css" />
+      <link rel="stylesheet" href="/public/bootstrap.min.css" />
     </head>
     <body>
       ${content}
