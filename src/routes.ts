b7e63654f4573646f73dc8f109ff3ed3c14adf1b
Ray <raypeaslee@gmail.com>
2024-12-24 13:25:40 -0500

    Update routes.ts
    add the blank page route

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import express from 'express'
 import { getIronSession } from 'iron-session'
 import type { AppContext } from '#/index'
 import { home } from '#/pages/home'
+import { blank } from '#/pages/blank'
 import { login } from '#/pages/login'
 import { env } from '#/lib/env'
 import { page } from '#/lib/view'
@@ -270,6 +271,20 @@ export const createRouter = (ctx: AppContext) => {
       }
 
       return res.redirect('/')
+    })
+  )
+
+  // Blank page
+  router.get(
+    '/blank',
+    handler(async (req, res) => {
+      // If the user is signed in, get an agent which communicates with their server
+      const agent = await getSessionAgent(req, res, ctx)
+      // If the user is not logged in send them to the login page.
+      if (!agent) {
+        return res.type('html').send(page(login({})))
+      }
+      return res.type('html').send(page(blank({})))
     })
   )
 
