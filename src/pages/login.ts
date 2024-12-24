9db2ceb93f26fddc8b2a0855d5a4a28bc09c8810
Ray <raypeaslee@gmail.com>
2024-12-23 20:25:59 -0500

    Update login.ts
    adding some bootstrap classes

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -12,22 +12,22 @@ export function login(props: Props) {
 
 function content({ error }: Props) {
   return html`<div id="root">
-    <div id="header">
+    <div id="header" class="text-center">
       <h1>Statusphere</h1>
       <p>Set your status on the Atmosphere.</p>
     </div>
     <div class="container">
       <form action="/login" method="post" class="login-form">
-        <input
+        <input class="form-control mb-2"
           type="text"
           name="handle"
           placeholder="Enter your handle (eg alice.bsky.social)"
           required
         />
-        <button type="submit">Log in</button>
+        <button type="submit" class="btn btn-primary">Log in</button>
         ${error ? html`<p>Error: <i>${error}</i></p>` : undefined}
       </form>
-      <div class="signup-cta">
+      <div class="text-center">
         Don't have an account on the Atmosphere?
         <a href="https://bsky.app">Sign up for Bluesky</a> to create one now!
       </div>
