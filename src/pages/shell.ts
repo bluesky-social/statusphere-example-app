5dad3221e38039809227762a2e10b4422667871b
Ray <raypeaslee@gmail.com>
2024-12-24 11:05:41 -0500

    Update shell.ts
    add meta tags

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -3,6 +3,8 @@ import { type Hole, html } from '../lib/view'
 export function shell({ title, content }: { title: string; content: Hole }) {
   return html`<html>
     <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>${title}</title>
       <link rel="stylesheet" href="/public/bootstrap.min.css" />
     </head>
