cbaa1d8167a84b6bba33fc3a47018373f5a180c7
Ray <raypeaslee@gmail.com>
2024-12-23 20:13:44 -0500

    Update shell.ts
    clean up comments

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -1,5 +1,4 @@
 import { type Hole, html } from '../lib/view'
-//import "bootswatch/dist/united/bootstrap.min.css"
 
 export function shell({ title, content }: { title: string; content: Hole }) {
   return html`<html>
