4ed94d1f05cbe8ea6ef211a3bcdb455f3496d5e7
Ray <raypeaslee@gmail.com>
2024-12-28 13:29:25 -0500

    add dark mode js links

--- src/pages/shell.ts
+++ src/pages/shell.ts
@@ -3,6 +3,7 @@ import { type Hole, html } from '../lib/view'
 export function shell({ title, content }: { title: string; content: Hole }) {
   return html`<html>
     <head>
+      <script src="/public/js/color-modes.js"></script>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>${title}</title>
