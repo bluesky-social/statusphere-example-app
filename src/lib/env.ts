dfa0cb9fdf761da16e883883e775172e1c69e44b
Ray <raypeaslee@gmail.com>
2024-12-30 08:18:09 -0500

    Update env.ts
    add mongodb to env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,5 +12,6 @@ export const env = cleanEnv(process.env, {
   PORT: port({ devDefault: testOnly(3000) }),
   PUBLIC_URL: str({}),
   DB_PATH: str({ devDefault: ':memory:' }),
+  MONGO_URL: str({ devDefault: 'mongodb://localhost:27017/statusphere' }),
   COOKIE_SECRET: str({ devDefault: '00000000000000000000000000000000' }),
 })
