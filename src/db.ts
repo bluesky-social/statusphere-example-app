c17dc62bb2e466530b35f6d1b1d1ce39ff7d8aaf
Ray <raypeaslee@gmail.com>
2025-01-01 15:49:37 -0500

    Update db.ts
    removes everything except kysely and types

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,5 @@
-import SqliteDb from 'better-sqlite3'
 import {
   Kysely,
-  Migrator,
-  SqliteDialect,
-  Migration,
-  MigrationProvider,
 } from 'kysely'
 
 // Types
@@ -35,59 +30,5 @@ export type AuthState = {
 
 type AuthStateJson = string
 type AuthSessionJson = string
-
-// Migrations
-
-const migrations: Record<string, Migration> = {}
-
-const migrationProvider: MigrationProvider = {
-  async getMigrations() {
-    return migrations
-  },
-}
-
-migrations['001'] = {
-  async up(db: Kysely<unknown>) {
-    await db.schema
-      .createTable('status')
-      .addColumn('uri', 'varchar', (col) => col.primaryKey())
-      .addColumn('authorDid', 'varchar', (col) => col.notNull())
-      .addColumn('status', 'varchar', (col) => col.notNull())
-      .addColumn('createdAt', 'varchar', (col) => col.notNull())
-      .addColumn('indexedAt', 'varchar', (col) => col.notNull())
-      .execute()
-    await db.schema
-      .createTable('auth_session')
-      .addColumn('key', 'varchar', (col) => col.primaryKey())
-      .addColumn('session', 'varchar', (col) => col.notNull())
-      .execute()
-    await db.schema
-      .createTable('auth_state')
-      .addColumn('key', 'varchar', (col) => col.primaryKey())
-      .addColumn('state', 'varchar', (col) => col.notNull())
-      .execute()
-  },
-  async down(db: Kysely<unknown>) {
-    await db.schema.dropTable('auth_state').execute()
-    await db.schema.dropTable('auth_session').execute()
-    await db.schema.dropTable('status').execute()
-  },
-}
-
-// APIs
-
-export const createDb = (location: string): Database => {
-  return new Kysely<DatabaseSchema>({
-    dialect: new SqliteDialect({
-      database: new SqliteDb(location),
-    }),
-  })
-}
-
-export const migrateToLatest = async (db: Database) => {
-  const migrator = new Migrator({ db, provider: migrationProvider })
-  const { error } = await migrator.migrateToLatest()
-  if (error) throw error
-}
 
 export type Database = Kysely<DatabaseSchema>
