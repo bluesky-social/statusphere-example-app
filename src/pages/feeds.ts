01721d2be3a77e70171574e656532f811286745d
Ray <raypeaslee@gmail.com>
2025-01-09 18:26:32 -0500

    Update feeds.ts
    lets save what we have so far

--- src/pages/feeds.ts
+++ src/pages/feeds.ts
@@ -1,12 +1,11 @@
 import { AppBskyActorDefs } from "@atproto/api";
 import { html } from "../lib/view";
 import { shell } from "./shell";
-import type AppBskyActorDefsSavedFeedPrefv2 from "@atproto/api";
+import type { SavedFeed } from '@atproto/api/dist/client/types/app/bsky/actor/defs';
 
 type Props = { 
   error?: string;
-  savedFeeds?: AppBskyActorDefsSavedFeedPrefv2[];
-
+  items: SavedFeed[];
 };
 
 export function feeds(props: Props) {
@@ -16,15 +15,32 @@ export function feeds(props: Props) {
 	});
 }
 
-function content({ error }: Props) {
+function content({ error, items }: Props) {
 	return html`
-    <div id="header" class="text-center">
-      <h1>A New Feeds Page</h1>
-      <p>It's time to build your next page.</p>
+    <div id="header" class="text-center border-bottom border-primary">      
+      <p class= "fs-2"><i class="bi bi-caret-left-fill text-primary"></i>Feeds</p>    
     </div>
     <div class="container">
-      <div>
-        You can add your content here.
+      <div>        
+        ${items.map((item, i) => {
+                const getLastPart = (value: string) => value.substring(value.lastIndexOf('/') + 1);
+                return html`
+                  <div class="card mt-2">
+                  <div class="card-body">
+                    <button class="btn btn-primary">
+                    <i class="bi bi-list-stars"></i>
+                    </button>
+                    ${getLastPart(item.value)}
+                    <div>
+                    ${item.value}
+                    </div>
+                    ${item.type}
+                    ${item.pinned ? html`<i class="bi bi-pin-angle-fill"></i>` : ''}
+                    ${item.id} 
+                  </div>
+                  </div>
+                `;
+        })}
       </div>
     </div>
   `;
