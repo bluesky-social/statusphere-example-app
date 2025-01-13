3d515d487114ccb7cb5d4e397f81adb75e08aa57
Ray <raypeaslee@gmail.com>
2025-01-13 08:26:31 -0500

    Update blank.ts
    adds the blank page imports and get function

--- src/routes/blank.ts
+++ src/routes/blank.ts
@@ -1,1 +1,23 @@
-placeholder
+import express from 'express'
+import { handler, getSessionAgent } from './utils'
+import { AppContext } from '#/index'
+
+export const createBlankRouter = (ctx: AppContext) => {
+  const router = express.Router()
+  
+  // Blank page
+    router.get(
+        "/blank",
+        handler(async (req, res) => {
+            // If the user is signed in, get an agent which communicates with their server
+            const agent = await getSessionAgent(req, res, ctx);
+            // If the user is not logged in send them to the login page.
+            if (!agent) {
+                return res.type("html").send(page(login({})));
+            }
+            return res.type("html").send(page(blank({})));
+        }),
+    );
+
+  return router
+}
