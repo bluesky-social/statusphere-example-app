e858faa1c5041da465c364880e092d92929c501f
Ray <raypeaslee@gmail.com>
2025-01-21 12:02:26 -0500

    Update notifications.ts
    closer

--- src/pages/notifications.ts
+++ src/pages/notifications.ts
@@ -15,14 +15,22 @@ export function notifications(props: Props) {
 }
 
 function content({ error, notifications }: Props) {
-	return html`
-    <div id="header" class="text-center border-bottom border-primary">      
-      <p class= "fs-2"><i class="bi bi-caret-left-fill text-primary" onclick="history.back()"></i>Notifications</p>    
-    </div>
-    <div class="container">
-      <div>
-        Not yet but we're working on it.${notifications[0].author.displayName}
+  return html`
+  <div id="header" class="text-center border-bottom border-primary">      
+    <p class="fs-2"><i class="bi bi-caret-left-fill text-primary" onclick="history.back()"></i>Notifications</p>    
+  </div>
+  <div class="container">
+  ${notifications.map((notification: NotificationView , i: number) => {
+    return html`
+    <div class="row bg-light mt-3 border border-primary rounded">
+      <div class="col-2">
+        <img src="${notification.author.avatar}" class="img-fluid rounded-circle img-thumbnail" alt="${notification.author.displayName}" />
       </div>
-    </div>
+      <div class="col-5">${notification.author.displayName}</div>
+      <div class="col-5">${notification.reason}</div>
+      
+    </div>`;
+  })}
+  </div>
   `;
 }
